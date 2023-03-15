5084c3b82e27f89e4a68a7e632cb6be2a4d36195
Jeet Vora <66431585+jeetvora331@users.noreply.github.com>
2023-03-16 00:07:24 +0530

    Time: 39 ms (15.53%), Space: 13.8 MB (59.48%) - LeetHub

--- 0394-decode-string/0394-decode-string.py
+++ 0394-decode-string/0394-decode-string.py
@@ -1,5 +1,27 @@
 class Solution:
     def decodeString(self, s: str) -> str:
+        # Iterative solution with Stack
+        stack = []
+        for i in range(len(s)):
+            if s[i] != "]":
+                stack.append(s[i])
+            else:
+                curr = ""
+                while stack[-1] != "[":
+                    curr = stack.pop() + curr
+                stack.pop() # remove bracket
+                
+                k = ""
+                while stack and stack[-1].isnumeric():
+                    k = stack.pop()  + k
+                    
+                stack.append(int(k) * curr )
+            
+        return "".join(stack)
+                    
+        
+        
+        
         # Recurssive Solution 
         def helper(i, count, left):
             if i >= len(s):
