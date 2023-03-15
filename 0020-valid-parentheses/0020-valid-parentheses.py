43f3d91e17ff9841d6e967f1ec6f07a8b2862732
Jeet Vora <66431585+jeetvora331@users.noreply.github.com>
2023-03-16 01:53:34 +0530

    Time: 29 ms (84.07%), Space: 13.9 MB (17.18%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,33 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        # using Stack
+        stack = []
+        hmap = {
+            ")" : "(",
+            "]" : "[",
+            "}" : "{"
+        }
+        
+        for i in s:
+            if i in hmap:
+                if stack and stack[-1] == hmap[i]:
+                    stack.pop()
+                else:
+                    return False
+                
+            else:
+                stack.append(i)
+        return not stack
+        
+        
+        # Solution without stack
+        while True:
+            if "()" in s:
+                s = s.replace("()", "")
+            elif "{}" in s:
+                s = s.replace("{}", "")
+            elif "[]" in s:
+                s = s.replace("[]", "")
+
+            else:        
+                return not s
