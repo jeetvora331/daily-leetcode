143a73ac7494c7c367cac169ba62e00e5e13e1e5
Jeet Vora <66431585+jeetvora331@users.noreply.github.com>
2023-03-15 23:52:29 +0530

    Time: 36 ms (31.38%), Space: 13.9 MB (59.48%) - LeetHub

---
+++ 0394-decode-string/0394-decode-string.py
@@ -0,0 +1,22 @@
+class Solution:
+    def decodeString(self, s: str) -> str:
+        def helper(i, count, left):
+            if i >= len(s):
+                return ""
+
+            if s[i] == "]":
+                left[0] = i
+                return ""
+
+            if s[i] == "[":
+                return int(count) * helper(i + 1, "", left) + helper(left[0] + 1, "", left)
+
+            if s[i].isnumeric():
+                return helper(i + 1, count + s[i], left)
+
+            else:
+                return s[i] + helper(i + 1, count, left)
+
+        left = [0]
+        return helper(0, "", left)
+        
