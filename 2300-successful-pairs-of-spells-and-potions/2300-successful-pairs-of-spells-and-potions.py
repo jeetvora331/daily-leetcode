1d91cab3cdedfcdd1bc37e49106a24a1c0c62c25
Jeet Vora <66431585+jeetvora331@users.noreply.github.com>
2023-04-02 20:42:03 +0530

    Time: 2024 ms (28.89%), Space: 37.1 MB (69.69%) - LeetHub

--- 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.py
+++ 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.py
@@ -1,18 +1,40 @@
 class Solution:
     def successfulPairs(self, spells: List[int], potions: List[int], success: int) -> List[int]:
-        sortedSpells = [(spell, index) for index, spell in enumerate(spells)]
-
-        
-        sortedSpells.sort()
+        # NEET CODE solution:
         potions.sort()
+        res = []
+        for s in spells:
+            
+            L = 0
+            R = len(potions) -1
+            idx = len(potions)
+            while L <=R:
+                    m = (L+R)//2
+                    
+                    if s * potions[m] >= success:
+                        R = m - 1
+                        idx = m
+                    else:
+                        L = m +1
+                
+            res.append(len(potions) - idx)
+        return res
         
-        answer = [0] * len(spells)
-        m = len(potions)
-        potionIndex = m - 1
         
-        for spell, index in sortedSpells:
-            while potionIndex >= 0 and (spell * potions[potionIndex]) >= success:
-                potionIndex -= 1
-            answer[index] = m - (potionIndex + 1)
         
-        return answer
+        #leet code solution:
+#         sortedSpells = [(spell, index) for index, spell in enumerate(spells)]
+    
+#         sortedSpells.sort()
+#         potions.sort()
+        
+#         answer = [0] * len(spells)
+#         m = len(potions)
+#         potionIndex = m - 1
+        
+#         for spell, index in sortedSpells:
+#             while potionIndex >= 0 and (spell * potions[potionIndex]) >= success:
+#                 potionIndex -= 1
+#             answer[index] = m - (potionIndex + 1)
+        
+#         return answer
