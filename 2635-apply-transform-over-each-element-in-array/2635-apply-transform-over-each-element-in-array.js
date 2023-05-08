ecc5af277d6ea89150896cc44a6fba9bf5db41e7
Jeet Vora <66431585+jeetvora331@users.noreply.github.com>
2023-05-08 20:13:12 +0530

    Time: 69 ms (6.58%), Space: 42.2 MB (37.61%) - LeetHub

--- 2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.js
+++ 2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.js
@@ -4,9 +4,9 @@
  * @return {number[]}
  */
 var map = function(arr, fn) {
-    for (let i =0 ; i<arr.length ; i++){
-        arr[i] = fn(arr[i], i)
+    const newArr = new Array(arr.length);
+    for (let i = 0; i < arr.length; ++i) {
+        newArr[i] = fn(arr[i], i);
     }
-    return arr
-    
+    return newArr;
 };
