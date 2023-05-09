da3245fd3cb55cb9d8b46db2a3920f0c86cc5141
Jeet Vora <66431585+jeetvora331@users.noreply.github.com>
2023-05-09 05:55:50 +0530

    Time: 56 ms (5.34%), Space: 16.2 MB (14.84%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.py
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -1,36 +1,31 @@
 class Solution:
-    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        l=0
-        r = len(matrix[0])
-        b = len(matrix)
-        t=0
+    def spiralOrder(self, mat: List[List[int]]) -> List[int]:
         res = []
+        sr = 0
+        er = len(mat) - 1 
         
-        while l<r and t<b:
-            for i in range(l,r):
-                res.append(matrix[t][i])
-            t=t+1
+        sc = 0
+        ec = len(mat[0]) - 1
+        
+        while sr <= er and sc <= ec:
+            for col in range(sc, ec + 1):
+                res.append(mat[sr][col])
                 
-            for i in range(t,b):
-                res.append(matrix[i][r-1])
-            r = r-1
+            for row in range(sr + 1, er + 1):
+                res.append(mat[row][ec])
             
-            if not (l<r and t<b):
-                break
-        
-            for i in range(r-1,l-1,-1):
-                res.append(matrix[b-1][i])
-            b = b-1
+            if sr != er:
+                for col in reversed(range(sc, ec)):
+                    res.append(mat[er][col])
+            
+            if sc != ec:
+                for row in reversed(range(sr + 1, er)):
+                    res.append(mat[row][sc])
                 
-            for i in range(b-1,t-1,-1):
-                res.append(matrix[i][l])
-                
-            l= l+1
+            sr += 1 
+            er -= 1
+            
+            sc += 1
+            ec -= 1
             
         return res
-        
-    
-                    
-        
-        
-        
