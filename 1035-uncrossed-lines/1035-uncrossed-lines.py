8c8c87af560ad2f1bb25eba6e1d5d795074ef8dd
Jeet Vora <66431585+jeetvora331@users.noreply.github.com>
2023-05-11 20:08:44 +0530

    Time: 489 ms (6.04%), Space: 61 MB (5.25%) - LeetHub

--- 1035-uncrossed-lines/1035-uncrossed-lines.py
+++ 1035-uncrossed-lines/1035-uncrossed-lines.py
@@ -2,18 +2,41 @@ class Solution:
     def maxUncrossedLines(self, nums1: List[int], nums2: List[int]) -> int:
         n = len(nums1)
         m = len(nums2)
+        def dfs (i,j):
+            if i == n or j == m:
+                return 0
+            
+            if (i,j) in dp:
+                return dp[(i,j)]
+            
+            if nums1[i]==nums2[j]:
+                dp[(i,j)] = 1 + dfs(i+1 , j+1)
+            
+            else:
+                dp[(i,j)]= max(dfs(i+1,j) , dfs(i,j+1))
+            
+            return dp[(i,j)]
         
-        dp = [ [0]*(m+1) for _ in range(n+1)]
+        dp = {}
+        
+            
+        
+        return dfs(0,0)
+############ DP solution #############        
+#         n = len(nums1)
+#         m = len(nums2)
+        
+#         dp = [ [0]*(m+1) for _ in range(n+1)]
         
         
-        for i in range(1, n+1):
-            for j in range(1, m+1):
-                if nums1[i-1] == nums2[j-1]:
-                    dp[i][j] = 1 + dp[i-1][j-1]
-                else:
-                    dp[i][j] =  max(dp[i-1][j], dp[i][j-1])
+#         for i in range(1, n+1):
+#             for j in range(1, m+1):
+#                 if nums1[i-1] == nums2[j-1]:
+#                     dp[i][j] = 1 + dp[i-1][j-1]
+#                 else:
+#                     dp[i][j] =  max(dp[i-1][j], dp[i][j-1])
         
-        return dp[n][m]  
+#         return dp[n][m]  
 
 #         n1 = len(nums1)
 #         n2 = len(nums2)
