8008db114ef8f42b2bdaeccbf9190aee5774801d
Jeet Vora <66431585+jeetvora331@users.noreply.github.com>
2023-05-19 04:30:59 +0530

    Time: 70 ms (9.39%), Space: 42.5 MB (9.63%) - LeetHub

--- 2622-cache-with-time-limit/2622-cache-with-time-limit.js
+++ 2622-cache-with-time-limit/2622-cache-with-time-limit.js
@@ -1,44 +1,28 @@
-function TimeLimitedCache() {
-  this.cache = new Map();
-}
+class TimeLimitedCache {
+    cache = new Map()
 
-/** 
- * @param {number} key
- * @param {number} value
- * @param {number} time until expiration in ms
- * @return {boolean} if un-expired key already existed
- */
-TimeLimitedCache.prototype.set = function(key, value, duration) {
-    const cacheValue = this.cache.get(key)
+    set (key, value, duration) {
+      const cacheValue =    this.cache.get(key)
+      if (cacheValue) {
+          clearTimeout(cacheValue.timeoutId)
+      }
+        
+      const timeoutId = setTimeout(()=>{
+          this.cache.delete(key)
+      }, duration)
+    this.cache.set(key, {value,timeoutId})
+        return !!cacheValue
+    };
+
     
-    if (cacheValue){
-        clearTimeout(cacheValue.tout)
+    get(key){
+        if (this.cache.has(key)){
+            return this.cache.get(key).value
+        }
+        return -1
     }
     
-    const tout = setTimeout(()=> this.cache.delete(key) , duration)
-    this.cache.set(key, {value , tout})
-    return Boolean(cacheValue)
-};
-
-/** 
- * @param {number} key
- * @return {number} value associated with key
- */
-TimeLimitedCache.prototype.get = function(key) {
-    return this.cache.has(key) ? this.cache.get(key).value : -1
-};
-
-/** 
- * @return {number} count of non-expired keys
- */
-TimeLimitedCache.prototype.count = function() {
-    return this.cache.size
-};
-
-/**
- * Your TimeLimitedCache object will be instantiated and called as such:
- * var obj = new TimeLimitedCache()
- * obj.set(1, 42, 1000); // false
- * obj.get(1) // 42
- * obj.count() // 1
- */
+    count(){
+        return this.cache.size
+    }
+}
