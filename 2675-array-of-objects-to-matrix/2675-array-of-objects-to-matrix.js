43c1e625d48af85de3827d64032a41a07592bedf
Jeet Vora <66431585+jeetvora331@users.noreply.github.com>
2023-05-23 20:49:06 +0530

    Time: 352 ms (88.04%), Space: 91.1 MB (96.35%) - LeetHub

---
+++ 2675-array-of-objects-to-matrix/2675-array-of-objects-to-matrix.js
@@ -0,0 +1,57 @@
+/**
+ * @param {Array} arr
+ * @return {Matrix}
+ */
+var jsonToMatrix = function(arr) {
+  const keySet = new Set();
+
+  for (const obj of arr) {
+    getKeys(obj, "");
+  }
+
+  const keys = Array.from(keySet).sort();
+  const res = [keys];
+
+  for (const obj of arr) {
+    const keyToVal = {};
+    getValues(obj, "", keyToVal);
+    let row = [];
+    for (const key of keys) {
+      if (key in keyToVal) {
+        // recursion
+        row.push(keyToVal[key]);
+      } else {
+        row.push("");
+      }
+    }
+    res.push(row);
+  }
+  return res;
+
+  function getKeys(obj, path) {
+    for (const key in obj) {
+      const newPath = path ? path + "." + key : key;
+      if (isObject(obj[key])) {
+        // recursion
+        getKeys(obj[key], newPath);
+      } else {
+        keySet.add(newPath);
+      }
+    }
+  }
+
+  function getValues(obj, path, keyToVal) {
+    for (const key in obj) {
+      const newPath = path ? path + "." + key : key;
+      if (isObject(obj[key])) {
+        getValues(obj[key], newPath, keyToVal);
+      } else {
+        keyToVal[newPath] = obj[key];
+      }
+    }
+  }
+
+  function isObject(obj) {
+    return obj !== null && typeof obj === "object";
+  }
+};
