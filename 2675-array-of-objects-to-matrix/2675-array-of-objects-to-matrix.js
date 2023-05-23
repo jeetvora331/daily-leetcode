b2fea1e434c15e06f69ad37cb4fa08f574ed9560
Jeet Vora <66431585+jeetvora331@users.noreply.github.com>
2023-05-23 21:04:03 +0530

    Time: 361 ms (85.38%), Space: 93.8 MB (86.05%) - LeetHub

--- 2675-array-of-objects-to-matrix/2675-array-of-objects-to-matrix.js
+++ 2675-array-of-objects-to-matrix/2675-array-of-objects-to-matrix.js
@@ -2,56 +2,56 @@
  * @param {Array} arr
  * @return {Matrix}
  */
-var jsonToMatrix = function(arr) {
-  const keySet = new Set();
+var jsonToMatrix = function (arr) {
+	const keySet = new Set();
 
-  for (const obj of arr) {
-    getKeys(obj, "");
-  }
+	for (const obj of arr) {
+		getKeys(obj, "");
+	}
 
-  const keys = Array.from(keySet).sort();
-  const res = [keys];
+	const keys = Array.from(keySet).sort();
+	const res = [keys];
 
-  for (const obj of arr) {
-    const keyToVal = {};
-    getValues(obj, "", keyToVal);
-    let row = [];
-    for (const key of keys) {
-      if (key in keyToVal) {
-        // recursion
-        row.push(keyToVal[key]);
-      } else {
-        row.push("");
-      }
-    }
-    res.push(row);
-  }
-  return res;
+	for (const obj of arr) {
+		const keyToVal = {};
+		getValues(obj, "", keyToVal);
+		let row = [];
+		for (const key of keys) {
+			if (key in keyToVal) {
+				//rec
+				row.push(keyToVal[key]);
+			} else {
+				row.push("");
+			}
+		}
+		res.push(row);
+	}
+	return res;
 
-  function getKeys(obj, path) {
-    for (const key in obj) {
-      const newPath = path ? path + "." + key : key;
-      if (isObject(obj[key])) {
-        // recursion
-        getKeys(obj[key], newPath);
-      } else {
-        keySet.add(newPath);
-      }
-    }
-  }
+	function getKeys(obj, path) {
+		for (const key in obj) {
+			const newPath = path ? path + "." + key : key;
+			if (isObject(obj[key])) {
+				//recursion
+				getKeys(obj[key], newPath);
+			} else {
+				keySet.add(newPath);
+			}
+		}
+	}
 
-  function getValues(obj, path, keyToVal) {
-    for (const key in obj) {
-      const newPath = path ? path + "." + key : key;
-      if (isObject(obj[key])) {
-        getValues(obj[key], newPath, keyToVal);
-      } else {
-        keyToVal[newPath] = obj[key];
-      }
-    }
-  }
+	function getValues(obj, path, keyToVal) {
+		for (const key in obj) {
+			const newPath = path ? path + "." + key : key;
+			if (isObject(obj[key])) {
+				getValues(obj[key], newPath, keyToVal);
+			} else {
+				keyToVal[newPath] = obj[key];
+			}
+		}
+	}
 
-  function isObject(obj) {
-    return obj !== null && typeof obj === "object";
-  }
+	function isObject(obj) {
+		return obj !== null && typeof obj === "object";
+	}
 };
