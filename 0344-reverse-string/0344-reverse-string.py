3507c0ee05fc69e5a58f5231ac9cbd7a53eed6a0
Jeet Vora <66431585+jeetvora331@users.noreply.github.com>
2023-07-09 03:32:29 +0530

    Time: 233 ms (16.72%), Space: 20.8 MB (55.12%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -4,5 +4,10 @@ class Solution:
         Do not return anything, modify s in-place instead
         """
         
-        s[::]=s[::-1]
+        # s[::]=s[::-1] #oneliner solution 
+        
+        for i in range(len(s)//2):
+            j = len(s) - 1  - i
+            
+            s[i], s[j] = s[j], s[i]
         
