ee01996328b67245e912f4ac4a53dbbd02bd2905
Jeet Vora <66431585+jeetvora331@users.noreply.github.com>
2023-07-09 01:20:13 +0530

    Time: 57 ms (53.31%), Space: 16.3 MB (74.97%) - LeetHub

--- 0763-partition-labels/0763-partition-labels.py
+++ 0763-partition-labels/0763-partition-labels.py
@@ -1,21 +1,39 @@
 class Solution:
-    def partitionLabels(self, S: str) -> List[int]:
-        count = {}
+    def partitionLabels(self, s: str) -> List[int]:
+        lastIdx = {}
+        
+        for i, c in enumerate(s):
+            lastIdx[c] = i
+            
         res = []
-        i, length = 0, len(S)
-        for j in range(length):
-            c = S[j]
-            count[c] = j
-
-        curLen = 0
-        goal = 0
-        while i < length:
-            c = S[i]
-            goal = max(goal, count[c])
-            curLen += 1
-
-            if goal == i:
-                res.append(curLen)
-                curLen = 0
-            i += 1
+        size = 0
+        end = 0
+        
+        for i , c in enumerate(s):
+            size +=1
+            end = max(end , lastIdx[c])
+            
+            if i == end:
+                res.append(size)
+                size = 0
         return res
+        
+#         count = {}
+#         res = []
+#         i, length = 0, len(S)
+#         for j in range(length):
+#             c = S[j]
+#             count[c] = j
+
+#         curLen = 0
+#         goal = 0
+#         while i < length:
+#             c = S[i]
+#             goal = max(goal, count[c])
+#             curLen += 1
+
+#             if goal == i:
+#                 res.append(curLen)
+#                 curLen = 0
+#             i += 1
+#         return res
