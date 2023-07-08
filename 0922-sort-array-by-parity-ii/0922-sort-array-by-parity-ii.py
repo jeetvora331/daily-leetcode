299900f266bb8cdf0bc9a47d293c009e4a7701f5
Jeet Vora <66431585+jeetvora331@users.noreply.github.com>
2023-07-09 03:28:33 +0530

    Time: 216 ms (53.61%), Space: 18.8 MB (53.49%) - LeetHub

--- 0922-sort-array-by-parity-ii/0922-sort-array-by-parity-ii.py
+++ 0922-sort-array-by-parity-ii/0922-sort-array-by-parity-ii.py
@@ -5,12 +5,12 @@ class Solution:
         
         while E < len(nums) and O < len(nums):
             if nums[E] % 2 == 0:
-                E += 2
-            elif nums[O] % 2 == 0:
-                nums[O], nums[E] = nums[E], nums[O]  # Use assignment operator (=) instead of comparison operator (==)
-                E += 2
-                O += 2
+                E+=2
+            elif nums[O] %2:
+                O+=2
             else:
-                O += 2  # Increment O if neither E nor O is even
-        
+                nums[O], nums[E] = nums[E], nums[O]
+                
+                E+=2
+                O+=2
         return nums
