c16acf8f80028b93940d71d753bf9f38cb982f87
Jeet Vora <66431585+jeetvora331@users.noreply.github.com>
2023-07-10 02:48:51 +0530

    Time: 177 ms (38.78%), Space: 31.6 MB (69.54%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
@@ -6,33 +6,50 @@
 
 class Solution:
     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
-        c1 = 0
-        c2 = 0
-        l1 = headA
-        l2 = headB
-        
-        while l1:
-            l1 = l1.next
-            c1 +=1
+
+#         NeetCode Solution
+        l1, l2 = headA, headB
+        while l1 != l2:
+            if l1:
+                l1 = l1.next
+            else:
+                l1 = headB
+
+            if l2:
+                l2 = l2.next
+            else:
+                l2 = headA
+        return l1
             
-        while l2:
-            l2 = l2.next
-            c2 +=1
         
-        if c2 > c1:
-            while c2 !=c1:
-                headB = headB.next
-                c2 -=1
+        # My Solution 
+#         c1 = 0
+#         c2 = 0
+#         l1 = headA
+#         l2 = headB
         
-        if c1 > c2:
-            while c2 !=c1:
-                headA = headA.next
-                c1 -=1
+#         while l1:
+#             l1 = l1.next
+#             c1 +=1
+            
+#         while l2:
+#             l2 = l2.next
+#             c2 +=1
+        
+#         if c2 > c1:
+#             while c2 !=c1:
+#                 headB = headB.next
+#                 c2 -=1
+        
+#         if c1 > c2:
+#             while c2 !=c1:
+#                 headA = headA.next
+#                 c1 -=1
                 
-        while headA:
-            if headA == headB:
-                return headA
-            headA = headA.next
-            headB = headB.next
+#         while headA:
+#             if headA == headB:
+#                 return headA
+#             headA = headA.next
+#             headB = headB.next
             
         
