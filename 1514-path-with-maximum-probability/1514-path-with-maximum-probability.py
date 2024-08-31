603426a6fa2e4899aa340b877f6bebe24f6e28d4
Jeet Vora <66431585+jeetvora331@users.noreply.github.com>
2024-08-31 05:35:56 +0530

    Time: 538 ms (77.64%), Space: 29.9 MB (7.04%) - LeetHub

--- 1514-path-with-maximum-probability/1514-path-with-maximum-probability.py
+++ 1514-path-with-maximum-probability/1514-path-with-maximum-probability.py
@@ -1,22 +1,25 @@
 class Solution:
-    def maxProbability(self, n: int, edges: List[List[int]], succProb: List[float], start: int, end: int) -> float:
-        max_prob = [0] * n
-        max_prob[start] = 1
+    def maxProbability(self, n: int, edges: List[List[int]], succProb: List[float], start_node: int, end_node: int) -> float:
+        dist = [-float('inf')] * n # Array for distances
+        adj = defaultdict(list) # Adjacency matrix
+
+        for i, edge in enumerate(edges):
+            u, v = edge
+            adj[u].append((v, succProb[i]))
+            adj[v].append((u, succProb[i]))
         
-        for i in range(n - 1):
-            # If there is no larger probability found during an entire round of updates,
-            # stop the update process.
-            has_update = 0
-            for j in range(len(edges)):
-                u, v = edges[j]
-                path_prob = succProb[j]
-                if max_prob[u] * path_prob > max_prob[v]:
-                    max_prob[v] = max_prob[u] * path_prob
-                    has_update = 1 
-                if max_prob[v] * path_prob > max_prob[u]:
-                    max_prob[u] = max_prob[v] * path_prob
-                    has_update = 1
-            if not has_update:
-                break
+        pq = [(-1, start_node)] # Max Heap - Start at start_node 
+        dist[start_node] = 0  # Distance from start_node to itself
+
+        while pq:
+            prob, node = heapq.heappop(pq)
+            prob = -prob
+            if node == end_node: # If we found end_node
+                return prob
+            for neighbor, cost in adj[node]:
+                newProb = prob * cost # Multiply for the new probability
+                if newProb > dist[neighbor]: # Only add to the heap if new distance is greater than previous
+                    dist[neighbor] = newProb
+                    heapq.heappush(pq, (-newProb, neighbor))
         
-        return max_prob[end]
+        return 0 # If no path from start_node to end_node
